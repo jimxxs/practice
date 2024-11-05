7f879e8629cc237bab344e911e0574948c5ac2a5
jim edward <jimssdward@gmail.com>
2024-11-05 09:58:37 +0300

    new file:   array manipulation and traversal/Max.java

---
+++ array manipulation and traversal/Max.java
@@ -0,0 +1,37 @@
+public class Max{
+int[][] array = {{3, 2, 1}, {4, 6, 5}};
+
+public int[] findMin(int[][] array) {
+    int min = Integer.MAX_VALUE;
+    int[] location = {-1, -1};
+    for (int i = 0; i < array.length; i++) {
+        for (int j = 0; j < array[i].length; j++) {
+            if (array[i][j] < min) {
+                min = array[i][j];
+                location = new int[]{i, j};
+            }
+        }
+    }
+    System.out.println("Min Value: " + min + " at " + java.util.Arrays.toString(location));
+    return location;
+}
+
+public int[] findMax(int[][] array) {
+    int max = Integer.MIN_VALUE;
+    int[] location = {-1, -1};
+    for (int i = 0; i < array.length; i++) {
+        for (int j = 0; j < array[i].length; j++) {
+            if (array[i][j] > max) {
+                max = array[i][j];
+                location = new int[]{i, j};
+            }
+        }
+    }
+    System.out.println("Max Value: " + max + " at " + java.util.Arrays.toString(location));
+    return location;
+}
+/*public static void main(String[] args) {
+    Max a = new Max();
+    
+}*/
+}
